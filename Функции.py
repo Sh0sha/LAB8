bfcadad8a29af268a73a3b8bdd7de8b1c399fd29
shohzod <siberf@mail.ru>
2024-02-19 12:55:52 +0500

    second

---
+++ Функции.py
@@ -0,0 +1,24 @@
+def insertInToMid(lst, num, str):
+    """
+    Функция для вставки строки в середину списка.
+    
+     lst -- список строк
+    str -- строка для вставки
+    """
+    mid_index = len(lst) // 2
+    lst.insert(mid_index, num, str)    "добавил нам"
+    return lst
+
+def insertAtPosition(lst, position, num, str):
+    """
+    Функция для вставки строки в указанную позицию списка
+    lst -- список строк
+    position -- позиция для вставки строки
+    num -- число для проверки возможности вставки
+    str -- строка для вставки
+    """
+    if position <= len(lst):
+        lst.insert(position, str)
+        return lst
+    else:
+        return "operation is not possible" 
